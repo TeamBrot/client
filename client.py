98ccc0f79be0e0c9b721373801e2a9b2e1b0613d
Luca Diedrich <Luca.Diedrich@student.hpi.uni-potsdam.de>
2020-10-29 10:26:40 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,51 @@
+import asyncio
+import websockets
+import json
+
+#JSON to send for corresponding operations
+LEFT = '{"action":"turn_left"}'
+RIGHT = '{"action":"turn_right"}'
+SLOWER = '{"action":"slow_down"}'
+FASTER = '{"action":"speed_up"}'
+NOTHING = '{"action":"change_nothing"}' 
+
+
+async def connect():
+    uri = "ws://localhost:8080/spe_ed"
+    #conect to websocket
+    async with websockets.connect(uri) as websocket:
+        while(True):
+            # wait for data from server
+            rxData = await websocket.recv()
+            # use data to calculate next move
+            msg = useData(rxData)
+            # send next move back to server
+            await websocket.send(msg)
+
+
+def useData(data):
+    # load json data
+    d = json.loads(data)
+    # extract information from json (examples)
+    deadline = d["deadline"]
+    you = d["you"]
+    own_direction = d["players"][str(you)]["direction"]
+    #...
+
+    #print("Your dir:" + d["players"][str(you)]["direction"])
+    #print("Other dir:" + d["players"][str(you+1)]["direction"])
+
+    #move diagonally
+    if(own_direction == "right"):
+        return RIGHT
+    elif(own_direction == "down"):
+        return LEFT
+    
+    return NOTHING
+
+
+
+#main
+asyncio.get_event_loop().run_until_complete(connect())
+asyncio.get_event_loop().run_forever()
+print("Fertig")
