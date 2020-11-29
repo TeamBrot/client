5ce4743904034d6da863bdf013fd8733b9971768
Alexander Sohn <alexander.sohn642@gmail.com>
2020-11-29 15:51:03 +0100

    Improve auto_script.sh

--- auto_script.sh
+++ auto_script.sh
@@ -5,12 +5,15 @@ for i in {0..1000}
 do
     echo "Spiele Spiel Nummer $i"
     ../server/server &
-    pid=$!
+    spid=$!
     echo "Der Server wurde gestartet"
-    sleep 1
+    sleep 0.2
+    ./client speku &
+    cpid=$!
     ./client smart &
-    ./client smart 
-    sleep 1
-    kill $pid
-    sleep  1
+    c2pid=$!
+    wait $cpid
+    sleep 0.1
+    kill $spid
+    sleep  0.1
 done
