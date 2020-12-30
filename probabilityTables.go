9120209e06e1a4c7dfad7056f94a29afa099dcc4
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2020-12-30 17:03:26 +0100

    Move parameters to respective files

--- probabilityTables.go
+++ probabilityTables.go
@@ -6,6 +6,17 @@ import (
 	"time"
 )
 
+//Setting this value higher, makes makes us more confident, that we will visit a field and no one other does. Setting it lower does the exact opposite
+const myStartProbability = 1.0
+
+const othersStartProbability = 1.0
+
+//This const defines the maximal number of Turns simulateGame will try to process
+const maxSimDepth = 40
+
+//This const defines after how many processed players simulatePlayer will schedule a garbage Collection cycle. Lowering the value improves memory efficiency but has a performance impact
+const processedPlayersTillGC = 60000
+
 // Result :
 type Result struct {
 	Visits [][]uint16
