ec6eaa89b38bb52003c3bfc74bf16b5b6c17cfa8
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2020-12-31 23:09:10 +0100

    Create images in temporary directory

--- process_logs.py
+++ process_logs.py
@@ -1,5 +1,7 @@
 import sys
 import json
+import tempfile
+import os
 from PIL import Image, ImageDraw
 import ffmpeg
 
@@ -10,7 +12,7 @@ if len(sys.argv) < 2:
     print("no file given")
     exit(1)
 
-basename = sys.argv[1].removesuffix(".json") 
+basename = os.path.basename(sys.argv[1]).removesuffix(".json")
 
 with open(sys.argv[1]) as f:
     data = json.load(f)
@@ -18,19 +20,20 @@ with open(sys.argv[1]) as f:
 width = data["game"][0]["width"]
 height = data["game"][0]["height"]
 
-for index, status in enumerate(data["game"]):
-    im = Image.new("RGB", (width * SCALING, height * SCALING))
-    draw = ImageDraw.Draw(im)
-    filename = basename + "-" + str(index).zfill(4) + ".png"
-    for i,y in enumerate(status["cells"]):
-        for j,x in enumerate(y):
-            draw.rectangle([j*SCALING,i*SCALING,(j+1)*SCALING,(i+1)*SCALING], fill=COLORS[x])
-    im.save(filename)
-    print(filename)
+with tempfile.TemporaryDirectory() as tmpdir:
+    for index, status in enumerate(data["game"]):
+        im = Image.new("RGB", (width * SCALING, height * SCALING))
+        draw = ImageDraw.Draw(im)
+        filename = os.path.join(tmpdir, basename + "-" + str(index).zfill(4) + ".png")
+        for i,y in enumerate(status["cells"]):
+            for j,x in enumerate(y):
+                draw.rectangle([j*SCALING,i*SCALING,(j+1)*SCALING-1,(i+1)*SCALING-1], fill=COLORS[x])
+        im.save(filename)
+        print(filename)
 
-(
-    ffmpeg
-    .input(basename+'*.png', pattern_type='glob', framerate=30)
-    .output(basename + ".mp4")
-    .run()
-)
+    (
+        ffmpeg
+        .input(os.path.join(tmpdir, "*.png"), pattern_type='glob', framerate=10)
+        .output(basename + ".mp4")
+        .run()
+    )
