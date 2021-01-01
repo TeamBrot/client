99ac8ebaba5182e548bd8b5a6fbac6615d8d7ed5
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2021-01-01 10:53:49 +0100

    Update process script

--- process_logs.py
+++ process_logs.py
@@ -7,6 +7,20 @@ import ffmpeg
 
 SCALING = 16
 COLORS = ["#dddddd", "#ff0000", "#00ff00", "#0000ff", "#00ffff", "#ffff00", "#ff00ff", "#000000"]
+NUM_FINAL_FRAMES = 30
+FPS = 10
+
+def make_image(status, filename):
+    width = status["width"]
+    height = status["height"]
+    im = Image.new("RGB", (width * SCALING, height * SCALING))
+    draw = ImageDraw.Draw(im)
+    for i,y in enumerate(status["cells"]):
+        for j,x in enumerate(y):
+            draw.rectangle([j*SCALING,i*SCALING,(j+1)*SCALING-1,(i+1)*SCALING-1], fill=COLORS[x])
+    im.save(filename)
+    print(filename)
+
 
 if len(sys.argv) < 2:
     print("no file given")
@@ -17,23 +31,21 @@ basename = os.path.basename(sys.argv[1]).removesuffix(".json")
 with open(sys.argv[1]) as f:
     data = json.load(f)
 
-width = data["game"][0]["width"]
-height = data["game"][0]["height"]
 
 with tempfile.TemporaryDirectory() as tmpdir:
-    for index, status in enumerate(data["game"]):
-        im = Image.new("RGB", (width * SCALING, height * SCALING))
-        draw = ImageDraw.Draw(im)
+    index = 0
+    for status in data["game"]:
         filename = os.path.join(tmpdir, basename + "-" + str(index).zfill(4) + ".png")
-        for i,y in enumerate(status["cells"]):
-            for j,x in enumerate(y):
-                draw.rectangle([j*SCALING,i*SCALING,(j+1)*SCALING-1,(i+1)*SCALING-1], fill=COLORS[x])
-        im.save(filename)
-        print(filename)
+        make_image(status, filename)
+        index += 1
+    for _ in range(NUM_FINAL_FRAMES):
+        filename = os.path.join(tmpdir, basename + "-" + str(index).zfill(4) + ".png")
+        make_image(data["game"][-1], filename)
+        index += 1
 
     (
         ffmpeg
-        .input(os.path.join(tmpdir, "*.png"), pattern_type='glob', framerate=10)
+        .input(os.path.join(tmpdir, "*.png"), pattern_type='glob', framerate=FPS)
         .output(basename + ".mp4")
         .run()
     )
