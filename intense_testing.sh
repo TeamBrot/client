8e140e03e284580f80321ce992dd9bf4debf18cb
Alexander Sohn <alexander.sohn642@gmail.com>
2021-01-08 10:50:10 +0100

    Adapt parameters

--- intense_testing.sh
+++ intense_testing.sh
@@ -8,13 +8,13 @@ baseoutput="output"
 
 #When starting a combi Client it takes one of those values as combis
 probabilities=("1.0" "1.2" "1.4" "1.6" "1.8" "2.0" "2.2" "2.4")
-activations=("0.05" "0.01" "0.005" "0.001" "0.0005" "0.00001")
+activations=("0.01" "0.0075" "0.005" "0.0025" "0.001" "0.1")
 clients=("combi")
 numPlayers=("2")
 lengths=("10" "15" "25" "35" "40" "50" "70")
 offsets=("4")
 deadlines=("2")
-filterValues=("0.55" "0.6" "0.65" "0.7" "0.75" "0.8" "0.85")
+filterValues=("0.67" "0.68" "0.69" "0.7" "0.71" "0.72" "0.73")
 
 cd server
 go build .
